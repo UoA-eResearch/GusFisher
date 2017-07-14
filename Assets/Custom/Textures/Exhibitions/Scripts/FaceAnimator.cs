2166329ee38b6afb624f812bb8e60ddbb24e9c63
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-07-14 16:38:27 +1200

    backup simple face animator

---
+++ Assets/Custom/Textures/Exhibitions/Scripts/FaceAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceAnimator : MonoBehaviour {
+
+	public Material[] matArray;
+	public bool isAnimating;
+	public int index = 0;
+	public float updateDelay = .5f;
+	private float lastChange = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAnimating && Time.time - lastChange > updateDelay)
+		{
+			lastChange = Time.time;
+			GetComponent<Renderer>().material = matArray[++index % matArray.Length];
+		}
+	}
+}
