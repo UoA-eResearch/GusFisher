122d1c30ce7f056adb47785969362028a4d5a1a3
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-07-20 17:24:33 +1200

    have player avatar follow headset. scale based on user height

---
+++ Assets/Custom/Geo/Player/SuitedMan/FollowHead.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHead : MonoBehaviour {
+
+	public GameObject head;
+	public GameObject playermodel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (head.transform.rotation.eulerAngles.x < 45)
+		{
+			transform.rotation = Quaternion.Euler(0, head.transform.rotation.eulerAngles.y, 0);
+		}
+		transform.position = head.transform.position;
+		playermodel.transform.localPosition = new Vector3(0, -head.transform.position.y, -0.21f);
+		playermodel.transform.localScale = new Vector3(1, head.transform.position.y /2, 1);
+	}
+}
