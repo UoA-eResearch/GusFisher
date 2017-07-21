22dc5563b4380485f50ec5563909543a6bd8326a
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-07-21 16:36:47 +1200

    attempt at close hand script

---
+++ Assets/Custom/Geo/Player/Hands/CloseHand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class CloseHand : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var controller = this.GetComponentInParent<Hand>().controller;
+		anim.Play("Take 001", -1, controller.hairTriggerDelta);
+	}
+}
