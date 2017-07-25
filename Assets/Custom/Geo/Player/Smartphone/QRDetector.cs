5b63e3be374de1791258d88f863890001a5c78a9
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-07-26 11:43:05 +1200

    setup smartphone / back camera, detect when back camera sees QR code

---
+++ Assets/Custom/Geo/Player/Smartphone/QRDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QRDetector : MonoBehaviour {
+
+	public Collider QRCode;
+	public Camera backCam;
+	public TextMesh helpText;
+
+	private bool scanning = false;
+	
+	void Update ()
+	{
+		if (scanning)
+		{
+			var planes = GeometryUtility.CalculateFrustumPlanes(backCam);
+			if (GeometryUtility.TestPlanesAABB(planes, QRCode.bounds))
+			{
+				Debug.Log("QR code detected");
+				helpText.text = "QR code\ndetected";
+				StartCoroutine(PlaySequence());
+			}
+		}
+	}
+
+	private IEnumerator PlaySequence()
+	{
+		yield return new WaitForSeconds(5);
+		Debug.Log("show now");
+	}
+}
