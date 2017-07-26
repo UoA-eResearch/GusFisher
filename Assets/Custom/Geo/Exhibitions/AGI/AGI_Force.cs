9f8b822ddcb0d137c09cc7fab0e708c7c5a05974
Bianca <b.haux@auckland.ac.nz>
2017-07-26 14:40:01 +1200

    Drone wings.

--- Assets/Custom/Geo/Exhibitions/AGI/AGI_Force.cs
+++ Assets/Custom/Geo/Exhibitions/AGI/AGI_Force.cs
@@ -7,7 +7,7 @@ public class AGI_Force : MonoBehaviour {
 	private float force = 0.05f;
 	private Rigidbody rb;
 	private float radius = 1.15f;
-	private Vector3 centerPosition = new Vector3(-18.63854f, 3.287673f, -0.9795871f);
+	private Vector3 centerPosition = new Vector3(-18.43f, 3.81f, -5.46f);
 	private Vector3 direction;
 	private float distance;
 
