61d5ec54b71b4d899ce25cdf17223683389a47b2
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-07-27 17:29:36 +1200

    Hide headphones when on head

---
+++ Assets/Custom/Geo/Other Small Objects/SnapToHead.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToHead : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "HeadCollider")
+		{
+			foreach (var r in gameObject.GetComponentsInChildren<Renderer>())
+			{
+				r.enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.name == "HeadCollider")
+		{
+			foreach (var r in gameObject.GetComponentsInChildren<Renderer>())
+			{
+				r.enabled = true;
+			}
+		}
+	}
+}
